4afdc5740639ec040a4b6f921426a03addca0073
nyein-chan-soe <nyeinchansoe.blue@gmail.com>
2026-09-16 11:35:16 +0630

    Create main.js

---
+++ assets/css/assets/js/main.js
@@ -0,0 +1,14 @@
+```javascript
+const menuButton = document.querySelector(".menu-button");
+const navLinks = document.querySelector(".nav-links");
+
+menuButton.addEventListener("click", () => {
+
+  if (navLinks.style.display === "flex") {
+    navLinks.style.display = "none";
+  } else {
+    navLinks.style.display = "flex";
+  }
+
+});
+```
